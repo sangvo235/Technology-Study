5858026cfc3d44c5c711a257bf0a014608b87319
sangvo235 <sangvo235@gmail.com>
2024-05-29 00:39:21 +1000

    rename

--- LeetCode/1. Arrays & Hashing/1 Two Sum.js
+++ LeetCode/1. Arrays & Hashing/1 Two Sum.js
@@ -4,19 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const numberToIndex = {};
 
     for (let i = 0; i < nums.length; i++) {
+        numberToIndex[nums[i]] = i;
+    }
 
-        for (let j = i + 1; j < nums.length; j++) {
+    for (let i = 0; i < nums.length; i++) {
+        const numberNeeded = target - nums[i];
 
-            if (nums[i] + nums[j] == target) {
-                return [i, j];
-            }
-
+        // To ensure we don't use the same element twice
+        if (numberToIndex[numberNeeded] !== undefined 
+            && numberToIndex [numberNeeded] !== i) {
+                return [i, numberToIndex[numberNeeded]]
         }
     }
     
 };
 
-// Time Complexity: O(n^2) 
-// Space Complexity: O(1)
+// Two Pass - Hash Map
+// Time Complexity: O(n) 
+// Space Complexity: O(n)
