f66700f075e5b4e3747af3573ae1e90a05c1b7f1
sangvo235 <sangvo235@gmail.com>
2024-06-01 01:05:32 +1000

    add-solution

---
+++ LeetCode/4. Binary Search/704. Binary Search.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (target === nums[mid]) {
+            return mid;
+        } 
+
+        if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+};
+
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
