cd0d6ad660a8313ebd77797d0d5817b42bf88077
Sang Vo <sangvo235@gmail.com>
2025-03-03 00:07:22 +1100

    update-with-console-log

--- 2025/mergeSort.js
+++ 2025/mergeSort.js
@@ -1,12 +1,15 @@
 function mergeSort(arr) {
 
     if(arr.length < 2) {
-        return arr;
+        return arr; // base case
     }
 
     const mid = Math.floor(arr.length / 2);
     const leftArr = arr.slice(0, mid);
-    const rightArr = arr.slice (mid);
+    const rightArr = arr.slice(mid);
+
+    console.log("Splitting:", arr, "=>", leftArr, "and", rightArr);
+
     return merge(mergeSort(leftArr), mergeSort(rightArr));
 
 }
@@ -14,14 +17,22 @@ function mergeSort(arr) {
 function merge(leftArr, rightArr) {
     const sortedArr = [];
     
+    console.log("Merging:", leftArr, rightArr);
+
     while(leftArr.length && rightArr.length) {
+        console.log("Left array length:", leftArr.length);
+        console.log("Right array length:", rightArr.length);
+
         if(leftArr[0] <= rightArr[0]) {
-            sortedArr.push(leftArr.shift());   
+            sortedArr.push(leftArr.shift());
+            console.log("sortedArr left add:", sortedArr);
         } else {
             sortedArr.push(rightArr.shift());
+            console.log("sortedArr right add:", sortedArr);
         }
     }
 
+    console.log("Merged:", [...sortedArr, ...leftArr, ...rightArr]);
     return [...sortedArr, ...leftArr, ...rightArr];
 }
 
