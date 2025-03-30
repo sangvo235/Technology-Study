9d283dc8e552b772832e429c9cbe26c644464084
Sang Vo <sangvo235@gmail.com>
2025-03-30 18:07:00 +1100

    add-solution

---
+++ 2025/3. NeetCode/3. Stack/150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = [];
+
+    const operators = {
+        "+" : (num1, num2) => num1 + num2,
+        "-" : (num1, num2) => num1 - num2,
+        "*" : (num1, num2) => num1 * num2,
+        "/" : (num1, num2) => Math.trunc(num1 / num2)
+    };
+
+    for(token of tokens) {
+        if(operators[token]) {
+            let num2 = stack.pop();
+            let num1 = stack.pop();
+            
+            stack.push(operators[token](num1, num2));
+        } else {
+            stack.push(parseInt(token));
+        }   
+    }
+
+    return stack.pop();
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
