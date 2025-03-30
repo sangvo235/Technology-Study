72775b331ea6b661e243dfa2bd3cf292ac373de7
Sang Vo <sangvo235@gmail.com>
2025-03-30 15:06:50 +1100

    add-pop

--- 2025/1. Data Structures/stack.js
+++ 2025/1. Data Structures/stack.js
@@ -12,6 +12,18 @@ class Stack {
         return this.count - 1;
     }
 
+    // return and remove top element in stack
+    // return undefined if stack is empty 
+
+    pop() {
+        if(this.count === 0) return undefined;
+        let deleteItem = this.items[this.count - 1];
+        this.count--;
+        console.log(`${deleteItem} removed`)
+        return deleteItem;
+    }
+
+
 }
 
 const stack = new Stack();
@@ -19,3 +31,7 @@ const stack = new Stack();
 stack.push(100);
 stack.push(200);
 stack.push(300);
+stack.pop();
+stack.pop();
+stack.pop();
+console.log(stack.pop());
