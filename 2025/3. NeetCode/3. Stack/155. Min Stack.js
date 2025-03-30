4495dfbaf3f2d468b42ff9ca2894863f969a8256
Sang Vo <sangvo235@gmail.com>
2025-03-30 17:24:48 +1100

    add-solution

---
+++ 2025/3. NeetCode/3. Stack/155. Min Stack.js
@@ -0,0 +1,49 @@
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    this.minStack.push(Math.min(this.minStack.length > 0 ? this.minStack[this.minStack.length - 1] : Infinity, val));
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    if (!this.stack.length) return undefined;
+    this.minStack.pop();
+    return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];    
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+// Time Complexity: O(1)
+// Space Complexity: O(n)
