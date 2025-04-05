e0d0ab39889b0b6a95e01e89842a11c71201dc52
Sang Vo <sangvo235@gmail.com>
2025-04-05 23:27:54 +1100

    add-solution

---
+++ 2025/3. NeetCode/3. Stack/84. Largest Rectangle in Histogram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    heights.push(0);
+
+    let stack = [];
+    let maxArea = 0;
+
+    for(let i = 0; i < heights.length; i++) {
+        let heightStart = i;
+    
+        while (stack.length && stack[stack.length - 1][1] > heights[i]) {
+            let [position, height] = stack.pop();
+            maxArea = Math.max(maxArea, (i - position) * height);
+            heightStart = position;
+        }
+
+        stack.push([heightStart, heights[i]]);
+        console.log(stack);
+    }
+    return maxArea;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
