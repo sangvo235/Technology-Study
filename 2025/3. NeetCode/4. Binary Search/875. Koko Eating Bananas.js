949d0377de378918d3b61f3e4fc6fb2da4d9f165
Sang Vo <sangvo235@gmail.com>
2025-04-06 15:09:17 +1000

    add-solution

---
+++ 2025/3. NeetCode/4. Binary Search/875. Koko Eating Bananas.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    // minimum speed
+    let left = 1; 
+    // max speed 
+    let right = Math.max(...piles);
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let hours = 0;
+
+        for (let i = 0; i < piles.length; i++) {
+            hours += Math.ceil(piles[i] / mid);
+        }
+
+        if (hours > h) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+     
+    // return left because it will give you the minimum value
+    return left;
+
+};
+
+// Time Complexity: O(n * log m)
+// Space Complexity: O(1)
