ff869fff175f8cad14177ba54f71e0de78aa66af
Sang Vo <sangvo235@gmail.com>
2025-05-01 17:52:36 +1000

    add-solution

--- 2025/3. NeetCode/6. Linked List/206. Reverse Linked List.js
+++ 2025/3. NeetCode/6. Linked List/206. Reverse Linked List.js
@@ -1,2 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // 1. Track where we currently are.
+    // 2. temp variable to keep track of what's in front of the node. 
+    // 3. temp variable to keep track of what's behind the node. 
 
+    let curr = head;
+    let prev = null;
 
+    while (curr !== null) {
+        // step 2
+        let nextNode = curr.next;
+        // step 3
+        curr.next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
