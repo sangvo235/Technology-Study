27dfc43c50e1a821434bdabf8a963401874cc487
Sang Vo <sangvo235@gmail.com>
2025-05-06 11:43:58 +1000

    add-solution

---
+++ 2025/3. NeetCode/7. Trees/100a. Same Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    // checks
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+
+    if (p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    // if (p.val !== q.val)
+    return false;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
