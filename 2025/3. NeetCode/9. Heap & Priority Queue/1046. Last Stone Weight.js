444d49484044a9686b67d7505f3e17d1c3165c7e
Sang Vo <sangvo235@gmail.com>
2025-09-15 23:51:47 +1000

    add solution

---
+++ 2025/3. NeetCode/9. Heap & Priority Queue/1046. Last Stone Weight.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    const heap = new MaxPriorityQueue();
+
+    for(const stone of stones) heap.enqueue(stone);
+
+    while (heap.size() > 1) {
+        const y = heap.dequeue(); // heaviest
+        const x = heap.dequeue(); // second heaviest
+        if (y !== x) {
+            heap.enqueue(y - x);
+        }
+    }
+
+    return heap.size() === 0 ? 0 : heap.front();
+};
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(n)
